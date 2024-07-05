d3467eefb05d1f9a1467e219292c6b5003b2124f
ivan <ivanvaneck2000@gmail.com>
2024-07-06 00:05:25 +0300

    add media queries

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,20 +1,57 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
-  { title: "Homepage", path: "/" },
-  { title: "About", path: "/about" },
-  { title: "Contact", path: "/contact" },
-  { title: "Blog", path: "/blog" },
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
 ];
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
+
+  const session = true;
+  const isAdmin = true;
+
   return (
     <div className={styles.container}>
-      {links.map((link) => {
-        return <NavLink item={link} key={link.title} />;
-      })}
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+      </div>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
