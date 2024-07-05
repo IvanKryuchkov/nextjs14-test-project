314bf0e00650836f23d36c731c714fd46ccd387c
ivan <ivanvaneck2000@gmail.com>
2024-07-06 00:16:47 +0300

    update about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
+  // https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className={styles.imgContainer}>
+      <Image src="/about.png" alt="About Image" fill className={styles.img} />
+    </div>
+  );
+};
 
 export default AboutPage;
